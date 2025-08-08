2c09cdc0754906380a5075439dac46aa11572239
NVerschoor10 <nverschoor@mail.austral.edu.ar>
2025-08-08 12:50:03 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,11 @@
 def math():
     a = 57
     b = 7
+    print(a + b)
+    print(a - b)
+    print(a*b)
+    prom = a + b 
+    print(float(prom / 2))
+    print(int(a/b))
+    print(int(a%b))
+    print(float(a/b))
